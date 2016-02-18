715b7b92d71c9e376d04b1620cda12030633a1db
Joe O. <joseph.opseth@my.uwrf.edu>
2016-02-18 16:35:42 -0600

    Add subtract feature

--- PA1/pa1.cpp
+++ PA1/pa1.cpp
@@ -37,6 +37,23 @@ string vectorToString(const vector<int> &first);
 */
 vector<int> add(const vector<int> &first, const vector<int> &second);
 
+/*
+ * Find the difference between two integers.
+ * Returns a positive result.
+ */
+vector<int> diff(const vector<int> &first, const vector<int> &second);
+
+/*
+ * Compare the numbers in two vectors.
+ * Return 1 if the first is larger, -1 if the first is smaller,
+ * 0 if they are the same.
+ */
+int compare(const vector<int> &first, const vector<int> &second);
+
+// Remove leading zeroes from a vector
+// (Since integer is reversed, leading zeroes are at the end)
+void remove_zeroes(vector<int> &num);
+
 // verify that a string contains an unsigned integer
 bool verify_string(string);
 
@@ -47,8 +64,9 @@ void pause_237(bool);
 
 int main(void) {
 	vector<string> strings;
-	string first_display, second_display, result_display;
-	vector<int> first, second, result;
+	string first_display, second_display, result_display, diff_display, diff_sign;
+	vector<int> first, second, result, diff_result;
+	bool first_larger;
 
 	// read and verify strings
 	strings = get_strings(2);
@@ -71,6 +89,17 @@ int main(void) {
 	result_display = vectorToString(result);
 
 	cout << first_display << " + " << second_display << " = " << result_display << endl;
+
+	// subtract second number from first number
+	first_larger = (compare(first, second) >= 0);
+	diff_result = diff(first, second);
+	diff_display = vectorToString(diff_result);
+	if (first_larger)
+		diff_sign = "";
+	else
+		diff_sign = "-";
+
+	cout << first_display << " - " << second_display << " = " << diff_sign << diff_display << endl;
 
 	pause_237(true);
 
@@ -159,6 +188,69 @@ vector<string> get_strings(int num) {
 		}
 	}
     return strings;	
+}
+
+void remove_zeroes(vector<int> &num) {
+	int i = num.size() - 1;
+	while (i >= 0 && num[i] == 0) {
+		num.pop_back();
+	    i--;
+	}
+}
+
+int compare(const vector<int> &first, const vector<int> &second) {
+	if (first.size() > second.size()) {
+		return 1;
+	}
+	else if (second.size() > first.size()) {
+		return -1;
+	}
+	else {
+		for (int i = first.size() - 1; i >= 0; i--) {
+			if (first[i] > second[i]) {
+				return 1;
+			}
+			else if (second[i] > first[i]) {
+				return -1;
+			}
+		}
+		return 0;
+	}
+
+}
+
+vector<int> diff(const vector<int> &first, const vector<int> &second) {
+	vector<int> result;
+	int carry, max, min, diff_sum;
+	const vector<int> *largePtr, *smallPtr;
+	if (compare(first, second) > 0) {
+		largePtr = &first;
+		smallPtr = &second;
+	}
+	else {
+		largePtr = &second;
+		smallPtr = &first;
+	}
+	
+	carry = 0;
+	max = (*largePtr).size();
+	min = (*smallPtr).size();
+	for (int i = 0; i < max; i++) {
+		if (i < min) {
+			diff_sum = (*largePtr)[i] + carry - (*smallPtr)[i];
+		}
+		else {
+			diff_sum = (*largePtr)[i] + carry;
+		}
+		carry = 0;
+		if (diff_sum < 0) {
+			diff_sum = diff_sum + 10;
+			carry = -1;
+		}
+		result.push_back(diff_sum);
+	}
+	remove_zeroes(result);
+	return result;
 }
 
 
